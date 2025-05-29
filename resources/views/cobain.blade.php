52ec45d53e63b23d00e6369ae0f6b027b5397daf
Shanella <140229356+ShanellaChr@users.noreply.github.com>
2025-05-29 14:07:18 +0700

    Benerin master

--- resources/views/cobain.blade.php
+++ resources/views/cobain.blade.php
@@ -1,20 +1,16 @@
-@extends('components.master')
+<x-master>
+    <h3>Cek Bootstrap JS dengan Tooltip & Alert</h3>
 
-@section('content')
+    <!-- Tombol dengan Tooltip -->
+    <button type="button" class="btn btn-outline-info" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip muncul saat hover!">
+        Hover saya
+    </button>
 
-<h3>Cek Bootstrap JS dengan Tooltip & Alert</h3>
+    <h1>COBA GITHUB</h1>
 
-<!-- Tombol dengan Tooltip -->
-<button type="button" class="btn btn-outline-info" data-bs-toggle="tooltip" data-bs-placement="top" title="Tooltip muncul saat hover!">
-    Hover saya
-</button>
-
-<h1>COBA GITHUB</h1>
-
-<!-- Alert yang bisa di-close -->
-<div class="alert alert-warning alert-dismissible fade show mt-3" role="alert">
-    Ini adalah alert Bootstrap dengan tombol close!
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-
-@endsection
+    <!-- Alert yang bisa di-close -->
+    <div class="alert alert-warning alert-dismissible fade show mt-3" role="alert">
+        Ini adalah alert Bootstrap dengan tombol close!
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</x-master>
--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -6,6 +6,12 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-
+    <x-navbar></x-navbar>
+    <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        {{ $slot }}
+      </div>
+    </main>
+    <x-footer></x-footer>
 </body>
 </html>
