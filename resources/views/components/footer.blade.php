b14c4a71ba887d9364570b0ddc67862ca0d35a76
Yedija Hazael Tanudjaya <yedija.tanudjaya@binus.ac.id>
2025-05-29 13:51:50 +0700

    Makin Footer Requirement
    Making footer and inserted 4 image

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,22 @@
 <footer class="bg-light text-center py-3 mt-5">
-    <p class="mb-0">© 2025 Hometory. All rights reserved.</p>
+    <img class="footer-hometory" src="{{ asset('img/text only logo.png') }}" alt="">
+
+    <div style="padding-top:2vh">
+        <a href="">My Inventory</a>
+        <a href="">Calendar</a>
+        <a href="">Statistic</a>
+        <a href="">Wishlist</a>
+        <a href="">Article</a>
+    </div>
+        
+    <div class="footer-icon">
+        <img style="padding-inline: 1vw" src="{{ asset('img/Instagram Icon.png') }}" alt="">
+        <img style="padding-inline: 1vw" src="{{ asset('img/Facebook Icon.png') }}" alt="">
+        <img style="padding-inline: 1vw" src="{{ asset('img/Whatsapp Icon.png') }}" alt="">
+    </div>
+
+    <p>A stock recording platform that makes it easy for you to manage stock quickly, <br> accurately and efficiently.</p>
+
+    <p>© 2025 CASHH. All Rights Reserved.</p>
+
 </footer>
