a270f00624e3a658a09aa8751d5ffc50b3cdec62
investwl <williangjym@gmail.com>
2025-05-31 21:58:05 +0700

    Revised the Title Template
    Meskipun ini cuma ngeupdate title, anggep aja ini commit untuk ngeupdate commit sebelumnya, update title supaya bisa dipakai dan dicopy ke page lain yang butuh.
    
    Yang berubah adalah pemakaian css di app.css supaya bootstrap beneran kepakai.
    
    Note : masih ada pakai app.css, untuk sementara ada class "title-color" untuk pakai warna yang spesifik untuk title ini (bootstrap nggak sediain)

--- resources/views/calendar/calendarPage.blade.php
+++ resources/views/calendar/calendarPage.blade.php
@@ -1,10 +1,10 @@
 <x-master>
 
-    <div class="title-container">
-        <h3 class="montserrat-bold title-my">My</h3>
-        <h3 class="montserrat-bold title-custom">Calendar</h3>
+    <div class="d-flex justify-content-center align-items-center pt-7">
+        <h3 class="montserrat-bold justify-content-center align-items-center fs-1">My</h3>
+        <h3 class="montserrat-bold justify-content-center align-items-center ms-2 fs-1 title-color">Calendar</h3>
     </div>
 
-    <div class="nunito-regular subtitle">Track your expires, track smarter!</div>
+    <div class="nunito-regular d-flex justify-content-center align-items-center">Track your expires, track smarter!</div>
 
 </x-master>
