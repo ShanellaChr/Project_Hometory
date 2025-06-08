af728c8dd419baf1b4868cf1dd600548ea4c9902
Yehezkiel Michael <yehezkielmichaels@gmail.com>
2025-06-08 23:21:25 +0700

    DONE detail item page frontend

--- resources/views/myInventory/itemDetailPage.blade.php
+++ resources/views/myInventory/itemDetailPage.blade.php
@@ -17,10 +17,12 @@
 </head>
 <body>
     <div class="container-fluid p-0">
+        {{-- Back Button --}}
         <a href="../myInventory">
             <img src="/img/Back Button.svg" alt="Back Button" class="BackButton">
         </a>
 
+        {{-- Content --}}
         <div class="d-flex justify-content-center align-items-center flex-column my-5">
             <img src="{{ asset('img/Beverages - Dairy Products.svg') }}" alt="Beverages - Dairy Products" style="width: 18vw">
             
@@ -37,6 +39,7 @@
             </div>
 
             <div class="container">
+                {{-- Item Details --}}
                 <h1 class="mt-5 montserrat-semibold text-2xl">Item Details</h1>
                 <hr class="border-2 border-black rounded w-full my-2" style="opacity: 1;">
 
@@ -52,6 +55,7 @@
                     </div>
                 </div>
 
+                {{-- My Notes --}}
                 <h1 class="mt-5 montserrat-semibold text-2xl">My Notes</h1>
                 <hr class="border-2 border-black rounded w-full my-2" style="opacity: 1;">
                 <p class="nunito-medium text-xl">
@@ -59,8 +63,10 @@
                     Semuanya ditaro di kulkas dapur biar dingin, laci yang kedua. Kemarin beli di Indomaret, kek biasa.
                 </p>
 
+                {{-- Expiration Dates --}}
                 <h1 class="mt-5 montserrat-semibold text-2xl">Expiration Dates</h1>
                 <hr class="border-2 border-black rounded w-full my-2" style="opacity: 1;">
+
                 <div class="d-flex flex-column">
                     <div class="d-flex flex-row bg-ijopalette rounded-3 mt-3 me-3 px-4 py-2 shadow" style="width: 30vw">
                         <img src="{{ asset('img/expiredButton.svg') }}" alt="Expired Icon" class="mb-1" style="width: 2.5vw">
@@ -73,6 +79,24 @@
                         <h1 class="poppins-bold text-ijopalette text-2xl mx-5 mt-1 pt-2">05/06/2025</h1>
                         <p class="poppins-medium text-ijopalette text-base pt-3 ms-auto">2 items</p>
                     </div>
+                </div>
+            </div>
+
+            {{-- Buttons --}}
+            <div class="d-flex flex-row ms-auto me-5">
+                <div class="mx-4 mb-4 mt-5" style="width: fit-content">
+                    <a href = "../crudItemPage" class="btn-dark-blue rounded-3 shadow" style="padding-left: 3.25vw; padding-right: 2vw; padding-top: 1.5vw; padding-bottom: 1.5vw">
+                        <span class= "nunito-bold text-2xl text-center">Edit Item</span> 
+                        <img src="{{ asset('img/editButton.svg') }}" alt="Edit Icon" class="pencil-icon mb-1 ms-0 pe-3">
+                    </a>
+                </div>
+
+                <div class="me-5 mb-4 mt-5" style="width: fit-content%">
+                    {{-- nanti pathnya diatur lagi --}}
+                    <a href = "../myInventory/expiredItemPage" class="btn-merah rounded-3 shadow" style="padding-left: 3.25vw; padding-right: 2vw; padding-top: 1.5vw; padding-bottom: 1.5vw">
+                        <span class= "nunito-bold text-2xl text-center">Delete Item</span> 
+                        <img src="{{ asset('img/trashbin white icon.svg') }}" alt="Trash Bin Icon" class="pencil-icon mb-1 ms-0 pe-3">
+                    </a>
                 </div>
             </div>
         </div>
