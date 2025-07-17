a2f29a8ba0d97ebeec9688325781209d123eea07
Yehezkiel Michael <yehezkielmichaels@gmail.com>
2025-07-17 08:35:51 +0700

    progress stats cont
    asdoakwdoakwodka bug mulu

--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Statistic extends Model
 {
-    protected $fillable = ['category_id', 'month_year', 'total_items'];
+    protected $fillable = ['user_id', 'category_id', 'month_year', 'total_items'];
 
     protected $dates = ['month_year'];
 
     public function category()
     {
         return $this->belongsTo(Categories::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use App\Models\Item;
+use App\Observers\ItemObserver;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -20,5 +22,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Item::observe(ItemObserver::class);
     }
 }
