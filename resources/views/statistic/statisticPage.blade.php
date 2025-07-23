dd50181a956d8f6cb60a7a7bf1d8a132dfe6194c
FeliciaHan <feliciahandoyo1124@gmail.com>
2025-07-23 23:47:52 +0700

    Statistik Gambar

--- resources/views/statistic/statisticPage.blade.php
+++ resources/views/statistic/statisticPage.blade.php
@@ -113,7 +113,7 @@
 
                 dataset.forEach((arc, index) => {
                     // Hanya tampilkan gambar jika persentase > 0
-                    if (!dataValues[index] || dataValues[index] === 0) return;
+                    if (!dataValues[index] || dataValues[index] < 3) return;
                     const angle = (arc.startAngle + arc.endAngle) / 2;
                     const radius = (arc.outerRadius + arc.innerRadius) / 2;
                     const x = arc.x + Math.cos(angle) * radius;
